28da273a0f0b016a140b6b07c79f326a76369d59
Saad <saadksioui@gmail.com>
2024-05-06 16:42:29 +0100

    add route delete

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,11 @@ use App\Http\Controllers\EleveController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('eleves', EleveController::class);
+// Route::resource('eleves', EleveController::class);
 Route::get('/eleves', [EleveController::class, 'index'])->name('eleves.index');
 Route::get('/create', [EleveController::class, 'create'])->name('eleves.create');
 Route::post('/store', [EleveController::class, 'store'])->name('eleves.store');
 Route::get('/eleves/{id}', [EleveController::class, 'show'])->name('eleves.show');
 Route::get('/eleves/{id}/edit', [EleveController::class, 'edit'])->name('eleves.edit');
 Route::put('/eleves/{id}', [EleveController::class, 'update'])->name('eleves.update');
+Route::delete('/eleves/{id}', [EleveController::class, 'destroy'])->name('eleves.destroy');
